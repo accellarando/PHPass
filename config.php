eeeb3b3ac090423a07a2f2b8915c2d544622db1d
Ella Work <emoss@campusstore.utah.edu>
2021-02-08 16:44:55 -0700

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+//Use this to configure the database you're connecting to, as well as the absolute paths to the webpages being modified.
+
+$host	  = "localhost";	//hostname or IP address for MySQL server
+$user	  = "root";			//MySQL account username
+$password = "secret";		//MySQL account password
+$table	  = "insecure";		//the table we're fixing
+
+
+?>
